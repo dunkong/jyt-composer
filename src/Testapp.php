1fc72156d2df4da61ada5b35bb4d7188b6dc416e
dunkong <862391059@qq.com>
2022-07-19 18:13:55 +0800

    新增代码

---
+++ src/Testapp.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jyt\commposer;
+
+class Testapp
+{
+    public function dumpstr($str){
+        var_dump($str);
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ea2e0d3a80448c41e7a9bd354c80169f91a46752',
+        'name' => 'jyt/commposer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jyt/commposer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ea2e0d3a80448c41e7a9bd354c80169f91a46752',
+            'dev_requirement' => false,
+        ),
+    ),
+);
